37dbaf46a77736a7eba7f82dcbaacc612796fba5
henryyang42 <henryyang42@gmail.com>
2017-07-15 16:15:42 +0800

    Refine setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='ckip-segmenter',
+    version='1.0',
+    description='Ckip Segmenter',
+    long_description=long_description,
+    url='https://github.com/henryyang42/ckip-segmenter',
+    author='henryyang42',
+    author_email='henryyang42@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+    python_requires='>=3',
+    keywords='NLP,tokenizing,Chinese word segementation,part-of-speech tagging',
+    py_modules=["ckip"],
+)
